91a3412fedd8b0f69b6e72a93485e3fd2a9460cd
Margaret Le <margaretminhle@gmail.com>
2013-07-27 15:42:24 -0700

    reworked init

---
+++ gen/com/studicafe/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.studicafe;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ src/com/studicafe/WelcomeActivity.java
@@ -0,0 +1,33 @@
+package com.studicafe;
+
+import android.os.Bundle;
+import android.os.Handler;
+import android.app.Activity;
+import android.content.Intent;
+import android.view.Menu;
+
+public class WelcomeActivity extends Activity {
+
+    private final int DISPLAY_LENGTH = 2000;
+
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle icicle) {
+        super.onCreate(icicle);
+        setContentView(R.layout.activity_welcome);
+
+        /* New Handler to start the Menu-Activity 
+         * and close this Splash-Screen after some seconds.*/
+        new Handler().postDelayed(new Runnable(){
+            @Override
+            public void run() {
+                /* Create an Intent that will start the Menu-Activity. */
+                Intent mainIntent = new Intent(WelcomeActivity.this,LoginActivity.class);
+                WelcomeActivity.this.startActivity(mainIntent);
+                //overridePendingTransition(R.anim.fade, R.anim.hold);
+                finish();
+            }
+        }, DISPLAY_LENGTH);
+
+    }
+}
