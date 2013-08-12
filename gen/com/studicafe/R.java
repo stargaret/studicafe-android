ae50a2b07e0dd52081344e5ced253714c54b4acf
Eric Vogler <ericvogler@gmail.com>
2013-08-12 14:13:56 -0700

    Added drafts of screens 12, 13, & 14.

--- gen/com/studicafe/R.java
+++ gen/com/studicafe/R.java
@@ -11,7 +11,10 @@ public final class R {
     public static final class attr {
     }
     public static final class color {
-        public static final int titlecolor=0x7f070000;
+        public static final int background=0x7f060000;
+        public static final int highlighted_answer=0x7f060002;
+        public static final int pressed_question=0x7f060001;
+        public static final int titlecolor=0x7f060003;
     }
     public static final class dimen {
         /**  Default screen margins, per the Android Design guidelines. 
@@ -24,43 +27,83 @@ public final class R {
         public static final int activity_vertical_margin=0x7f040001;
     }
     public static final class drawable {
-        public static final int app_background=0x7f020000;
-        public static final int final_icon=0x7f020001;
-        public static final int ic_launcher=0x7f020002;
-        public static final int loading_screen=0x7f020003;
-        public static final int login_background=0x7f020004;
+        public static final int ap_biology_cafe=0x7f020000;
+        public static final int ap_chemistry_cafe=0x7f020001;
+        public static final int ap_enviro_cafe=0x7f020002;
+        public static final int ap_physics_cafe=0x7f020003;
+        public static final int app_background=0x7f020004;
+        public static final int avatar1=0x7f020005;
+        public static final int coffeejar2=0x7f020006;
+        public static final int color_selector=0x7f020007;
+        public static final int final_icon=0x7f020008;
+        public static final int free_response_flash_cards=0x7f020009;
+        public static final int gradient_background_selector=0x7f02000a;
+        public static final int ic_launcher=0x7f02000b;
+        public static final int loading_screen=0x7f02000c;
+        public static final int login_background=0x7f02000d;
+        public static final int multiple_choice_questions=0x7f02000e;
+        public static final int practice_tests=0x7f02000f;
+        public static final int question_background_color_selector=0x7f020010;
+        public static final int question_list_item_selector=0x7f020011;
+        public static final int question_normal_color=0x7f020016;
+        public static final int question_selected_color=0x7f020015;
+        public static final int statistics_button=0x7f020012;
+        public static final int sunrise=0x7f020013;
+        public static final int wrong_answer=0x7f020014;
     }
     public static final class id {
-        public static final int action_settings=0x7f090008;
-        public static final int editPassword=0x7f090001;
-        public static final int editUsername=0x7f090000;
-        public static final int login_button=0x7f090006;
-        public static final int registration=0x7f090005;
-        public static final int splashscreen=0x7f090007;
-        public static final int textView1=0x7f090004;
-        public static final int textView2=0x7f090002;
+        public static final int action_settings=0x7f090012;
+        public static final int editPassword=0x7f090004;
+        public static final int editUsername=0x7f090005;
+        public static final int imageButton1=0x7f090007;
+        public static final int imageButton2=0x7f090008;
+        public static final int imageButton3=0x7f090009;
+        public static final int imageButton4=0x7f09000c;
+        public static final int login_button=0x7f090002;
+        public static final int menu_settings=0x7f090011;
+        public static final int question_list=0x7f09000b;
+        public static final int question_list_question=0x7f09000a;
+        public static final int question_row_item=0x7f09000f;
+        public static final int question_row_radio_button=0x7f09000e;
+        public static final int question_row_x=0x7f090010;
+        public static final int registration=0x7f090006;
+        public static final int splashscreen=0x7f09000d;
+        public static final int textView1=0x7f090000;
+        public static final int textView2=0x7f090001;
         public static final int textView3=0x7f090003;
     }
     public static final class layout {
         public static final int activity_login=0x7f030000;
-        public static final int activity_registration=0x7f030001;
-        public static final int activity_welcome=0x7f030002;
+        public static final int activity_menu=0x7f030001;
+        public static final int activity_question_list=0x7f030002;
+        public static final int activity_registration=0x7f030003;
+        public static final int activity_sunrise=0x7f030004;
+        public static final int activity_welcome=0x7f030005;
+        public static final int question_list_row=0x7f030006;
     }
     public static final class menu {
-        public static final int login=0x7f080000;
-        public static final int registration=0x7f080001;
-        public static final int welcome=0x7f080002;
+        public static final int activity_menu=0x7f080000;
+        public static final int activity_question_list=0x7f080001;
+        public static final int activity_sunrise=0x7f080002;
+        public static final int login=0x7f080003;
+        public static final int registration=0x7f080004;
+        public static final int welcome=0x7f080005;
     }
     public static final class string {
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
         public static final int hello_world=0x7f050002;
         public static final int login=0x7f050007;
+        public static final int menu_settings=0x7f05000a;
         public static final int password=0x7f050006;
         public static final int register=0x7f050009;
         public static final int sign_in=0x7f050004;
         public static final int title_activity_login=0x7f050003;
+        public static final int title_activity_menu=0x7f05000e;
+        public static final int title_activity_question_list=0x7f05000b;
+        public static final int title_activity_question_list_adapter=0x7f05000c;
         public static final int title_activity_registration=0x7f050008;
+        public static final int title_activity_sunrise=0x7f05000d;
         public static final int username=0x7f050005;
     }
     public static final class style {
@@ -85,9 +128,9 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f060000;
+        public static final int AppBaseTheme=0x7f070000;
         /**  Application theme. 
          */
-        public static final int AppTheme=0x7f060001;
+        public static final int AppTheme=0x7f070001;
     }
 }
